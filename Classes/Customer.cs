ab8c0f1f2342e60498936333a4f9363317de198e
Nergis Ketenci <33528338+NrgsK@users.noreply.github.com>
2023-10-25 22:24:14 +0300

    changes

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -9,10 +9,19 @@ namespace Classes
     internal class Customer
     {
         // Bir müşterinin özelliklerini tutmak için kullanılan nesnedir.
-        public int Id { get; set; } // Property
-        public string FirstName { get; set; }
-        //public string FirstName;
+
+        public int Id { get; set; } // Property tanımlama
+        private string _firstName;
+        public string FirstName
+        {
+            get { return "Mr." + _firstName; }
+            set { _firstName = value; }
+        }
+        //Bir field üzerinde değişiklik yapmak istediğimizde bu yöntemi kullanırız.
+        //public string FirstName; //field - alan tanımlama
         public string LastName { get; set; }
         public string City { get; set; }
+
+        //Encapsulation
     }
 }
