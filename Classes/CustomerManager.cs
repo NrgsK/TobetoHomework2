a2bc10c887730db23a6f5caebf7a5b0e751eafc8
Nergis Ketenci <33528338+NrgsK@users.noreply.github.com>
2023-10-25 15:57:14 +0300

    Add project files.

---
+++ Classes/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class CustomerManager
+    {
+
+        //Müşteri ile ilgili işlemler.
+        public void Add() //Metot
+        {
+            Console.WriteLine("Customer Added!");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated!");
+        }
+
+    }
+
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Class amacı yapılmak istenen işlemleri gruplara ayırmak, bu gruplar üzerinden işlemler yapmak ve rahat bir şekilde bu gruplara ulaşmak.
+            //Sınıfları kullanabilmek için main de birer örnek oluşturmak gerekir.
+            CustomerManager customerManager = new CustomerManager(); // Class ın bir örneği oluşturuldu
+            customerManager.Add();
+            customerManager.Update();
+
+
+            ProductManager productManager = new ProductManager();
+            // Çalışacağımız nesne ile ilgili önce örnek oluşturuyoruz sonrasında içerisindeki metotları çağırıyoruz.
+            productManager.Add();
+            productManager.Update();
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+
+
+
+}
