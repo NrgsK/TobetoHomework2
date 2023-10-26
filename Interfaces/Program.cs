a79c3b3c3f67407591c8e89c78366291b1e0dc2b
Nergis Ketenci <33528338+NrgsK@users.noreply.github.com>
2023-10-26 21:39:56 +0300

    interface

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -11,6 +11,20 @@ namespace Interfaces
         static void Main(string[] args)
         {
             // Interface isimlendirme standardı "I" ile başlamasıdır.
+            PersonManager manager = new PersonManager();
+
+            //Customer customer= new Customer{ Id = 1, FirstName = "Nergis", LastName = "Ketenci", Adress = "Rize" };
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Furkan",
+                LastName = "Ketenci",
+                Department = "Science",
+            };
+            manager.Add(new Customer { Id = 1, FirstName = "Nergis", LastName = "Ketenci", Adress = "Rize" });
+            manager.Add(student);
+            Console.ReadLine();
         }
     }
     interface IPerson
@@ -18,16 +32,16 @@ namespace Interfaces
         // Soyut nesne - tek başına bir anlam ifade etmez.
         // interface nin amacı bir temel temel nesne oluşturup bütün nesneleri ondan implemente etmektir.
         int Id { get; set; }
-        int FirstName { get; set; }
-        int LastName { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
     }
     class Customer : IPerson // IPersonda tanımlanan herşeyi bu sınıflarda görebiliriz.
     {
         // Somut nesne
         // Interface de yazılan özelliklerin karşılığını implement edilen sınıfta tanımlamalısın.
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Adress { get; set; } // Müşteriye ait ayrı bir özellik
 
     }
@@ -35,8 +49,20 @@ namespace Interfaces
     {
         // Somut nesne
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Department { get; set; } // Öğrenciye ait ayrı bir özellik
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            // Bir nesne de parametre olabilir.
+            //public void Add(Customer customer) --> student gönderemiyoruz. Tanımladığımız IPerson interfacesini parametre olarak göndermeliyiz
+
+
+            Console.WriteLine(person.FirstName);
+        }
     }
 }
